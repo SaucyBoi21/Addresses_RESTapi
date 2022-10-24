6a715fd5c36661bb10df6dbcfa24a7146f0e32a2
Saahas Swaroop <saahas.swaroop@googlemail.com>
2022-10-24 12:34:45 -0400

    formating

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
 
 from resources.address import blp as AddressBlueprint
+
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -18,12 +20,15 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
-
     api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "test"
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
